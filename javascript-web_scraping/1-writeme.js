4f3cf6ae3241ced4487f2549ec971b2c2f80592b
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2024-05-08 16:37:49 +0000

    1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,9 +4,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const stringy = process.argv[3];
 
-try {
-  const content = fs.writeFile(filePath, stringy);
-  console.log(content);
-} catch (error) {
-  console.error(error);
-}
+fs.writeFile(filePath, stringy, (error) => {
+  if (error) {
+    console.error(error);
+  }
+});
